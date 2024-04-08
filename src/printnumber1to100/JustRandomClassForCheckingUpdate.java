e0b786db13f44be2dafc7c8d30644aa8307d22e5
Aditya Kumar <kumaraditya1501336@gmail.com>
2024-04-08 10:13:11 +0530

    Checking for updates

---
+++ src/printnumber1to100/JustRandomClassForCheckingUpdate.java
@@ -0,0 +1,7 @@
+package printnumber1to100;
+
+public class JustRandomClassForCheckingUpdate {
+    public static void main(String[] args) {
+        System.out.println("In JustRandomClassForCheckingUpdate");
+    }
+}
---
+++ src/printnumber1to100/JustRandomClassForCheckingUpdate2.java
@@ -0,0 +1,7 @@
+package printnumber1to100;
+
+public class JustRandomClassForCheckingUpdate2 {
+    public static void main(String[] args) {
+        System.out.println("In JustRandomClassForCheckingUpdate2");
+    }
+}
